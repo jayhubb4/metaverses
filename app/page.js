d952a44a9e83965158d28d2df8eaf765458a5665
Jonathan Hubbard <gsusplaya@gmail.com>
2023-01-23 23:58:16 -0500

    Finished explore section

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+// If you unnaturally see divs on this page, it is solely for the gradients. For the gradients to work, some components had to wrapped in divs. See below.
+
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
@@ -5,13 +7,22 @@ const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
-    <About />
-    <Explore />
-    <GetStarted />
-    <WhatsNew />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
+    <div className="relative">
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
+    </div>
     <World />
-    <Insights />
-    <Feedback />
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
+    </div>
     <Footer />
   </div>
 );
--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,24 +1,31 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { textContainer, textVariant2 } from "../utils/motion";
+import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
-    classname={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
   >
     {Array.from(title).map((letter, index) => (
       <motion.span
         variants={textVariant2}
         key={index}
       >
-        {Letter === ' ' ? '\u00A0' : Letter}
+        {letter === ' ' ? '\u00A0' : letter}
       </motion.span>
     ))}
   </motion.p>
 );
 
 export const TitleText = ({ title, textStyles }) => (
-  <h2>Title Text</h2>
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
 );
